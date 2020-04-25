990a1e12ea997a7dad91ad5c1345c9ea1758ea85
CharmingCharm <38855043+CharmingCharm@users.noreply.github.com>
2020-04-25 16:49:54 +0800

    Buid up basic project
    Add Login function and Register function

---
+++ Src/Code/user/urls.py
@@ -0,0 +1,28 @@
+"""UIC_3DLab URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from user import views
+
+app_name = 'user'
+urlpatterns = [
+    path('', views.index),
+    path('admin/', admin.site.urls),
+    path('login/', views.login_self),
+    path('register/', views.register),
+    path('logout/', views.logout_self),
+]
---
+++ Src/Code/user/views.py
@@ -0,0 +1,57 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import LoginForm, RegisterForm
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def login_self(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            messages.add_message(request, messages.ERROR, 'Username does not exist or password is wrong!')
+        return render(request, 'login.html', locals())
+    login_form = LoginForm()
+    return render(request, 'login.html', locals())
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            username = register_form.cleaned_data['username']
+            password = register_form.cleaned_data['password']
+            # confirm_password = register_form.cleaned_data['confirm_password']
+            email = register_form.cleaned_data['email']
+            # if password != confirm_password:
+            #     messages.add_message(request, messages.ERROR, 'Your confirm password is different from your password!')
+            #     return render(request, 'register.html', locals())
+            # else:
+            user_obj = User.objects.filter(username=username).first()
+            if not user_obj:
+                user = User.objects.create_user(username=username, password=password, email=email)
+                user.save()
+                messages.add_message(request, messages.SUCCESS, 'Register succeed, please Login!')
+                return redirect('/login/')
+            messages.add_message(request, messages.ERROR, 'Username already exists!')
+            return render(request, 'register.html', locals())
+    register_form = RegisterForm()
+    return render(request, 'register.html', locals())
+
+def logout_self(request):
+    logout(request)
+    messages.add_message(request, messages.SUCCESS, 'Logout successfully.')
+    return redirect("/")
