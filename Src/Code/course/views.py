a73a59fae234c5f1bde526894844465e4f7f7709
hixiaotian <1220219614@qq.com>
2020-04-29 23:46:54 +0800

    2020/4/29
    23:46

--- Src/Code/course/views.py
+++ Src/Code/course/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from user.models import User
+from .models import Course
 
-# Create your views here.
+
+def course_page(request, course_id):
+    user = User.objects.get(id=request.user.id)
+    course = Course.objects.get(id=course_id)
+    teacher = User.objects.filter(field='teacher', course=course_id)
+    return render(request, 'course.html', locals())
--- Src/Code/user/views.py
+++ Src/Code/user/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import LoginForm, RegisterForm
 from course.models import Course
+from django.views.decorators.csrf import csrf_exempt
+from django.core.paginator import Paginator
 
 def index(request):
     return render(request, 'index.html')
@@ -63,6 +65,63 @@ def logout_self(request):
 def student_home(request):
     user = User.objects.get(id=request.user.id)
     course = Course.objects.filter(member=request.user.id)
+    return render(request, 'mainpage_student.html', locals())
+
+
+@csrf_exempt
+def student_home(request):
+    user = User.objects.get(id=request.user.id)
+    course = Course.objects.filter(member=request.user.id).order_by('id')
+
+    p = Paginator(course, 5)
+    if p.num_pages <= 1:
+        course_list = course
+        data = ''
+    else:
+        page = int(request.GET.get('page', 1))
+        course_list = p.page(page)
+        left = []
+        right = []
+        left_has_more = False
+        right_has_more = False
+        first = False
+        last = False
+        total_pages = p.num_pages
+        page_range = p.page_range
+        if page == 1:
+            right = page_range[page:page + 2]
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+        elif page == total_pages:
+            left = page_range[(page - 3) if (page - 3) > 0 else 0:page - 1]
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+        else:
+            left = page_range[(page - 3) if (page - 3) > 0 else 0:page - 1]
+            right = page_range[page:page + 2]
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+        data = {
+            'left': left,
+            'right': right,
+            'left_has_more': left_has_more,
+            'right_has_more': right_has_more,
+            'first': first,
+            'last': last,
+            'total_pages': total_pages,
+            'page': page
+        }
+
     return render(request, 'mainpage_student.html', locals())
 
 def teacher_home(request):
