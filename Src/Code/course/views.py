88745cd3e4b88a51f2487923fb6a27683d0f5e5d
CharmingCharm <38855043+CharmingCharm@users.noreply.github.com>
2020-05-13 00:48:07 +0800

    Change the generate student

--- Src/Code/course/views.py
+++ Src/Code/course/views.py
@@ -141,7 +141,7 @@ def generate_student(request, course_id):
             user_list = request.session.pop('user_list')
             while len(user_list) > 0:
                 curr_user = user_list.pop()
-                user = User.objects.create_user(username=curr_user['user_name'], password=initial_pwd, email=curr_user['email'], field='student')
+                user = User.objects.create_user(username=curr_user['user_name'] ,truename=curr_user['true_name'], password=initial_pwd, email=curr_user['email'], field='student')
                 user.save()
                 student = Student.objects.create(studentID=str(curr_user['stu_id']), GPA=curr_user['GPA'], user=user)
                 student.save()
@@ -154,7 +154,7 @@ def generate_student(request, course_id):
 def import_student_excel(request, course_id):
     course = Course.objects.get(id=course_id)
     msg = "no_msg"
-    print(request.session.get('user_list'))
+    
     if request.method == 'POST':
         if len(request.FILES) != 0:
             excel_file = request.FILES.get('excel_file', '')
@@ -171,13 +171,14 @@ def import_student_excel(request, course_id):
 
                     for row in range(1, rows):
                         row_values = table.row_values(row)
+                        split_email = row_values[2].split("@")
+                        user_name = split_email[0]
                         if type(row_values[1]) == float:
-                            temp_curr_stu = {'user_name': row_values[0], 'stu_id': str(int(row_values[1])), 'email': row_values[2], 'GPA': row_values[3]}
+                            temp_curr_stu = {'user_name': user_name, 'true_name': row_values[0], 'stu_id': str(int(row_values[1])), 'email': row_values[2], 'GPA': row_values[3]}
                         else:
-                            temp_curr_stu = {'user_name': row_values[0], 'stu_id': str(row_values[1]), 'email': row_values[2], 'GPA': row_values[3]}
+                            temp_curr_stu = {'user_name': user_name, 'true_name': row_values[0], 'stu_id': str(row_values[1]), 'email': row_values[2], 'GPA': row_values[3]}
                         if Student.objects.filter(studentID=temp_curr_stu['stu_id']).first() is None:
                             temp_stu.append(temp_curr_stu)
-
                 request.session['temp_stu_excel'] = temp_stu
                 msg = 'success'
                 return redirect('/course/' + str(course.id) + '/generate_student')
@@ -193,10 +194,12 @@ def import_student_individual(request, course_id):
         inidividual_form = ImportIndividualForm(request.POST)
         if inidividual_form.is_valid():
             temp_user = {}
-            temp_user['user_name'] = inidividual_form.cleaned_data['user_name']
+            temp_user['true_name'] = inidividual_form.cleaned_data['true_name']
             temp_user['stu_id'] = inidividual_form.cleaned_data['stu_id']
             temp_user['email'] = inidividual_form.cleaned_data['email']
             temp_user['GPA'] = inidividual_form.cleaned_data['GPA']
+            split_email = temp_user['email'].split("@")
+            temp_user['user_name'] = split_email[0]
 
             if Student.objects.filter(studentID=temp_user['stu_id']).first() is None:
                 request.session['temp_user'] = temp_user
