18b1978d9723c5f69562ceb97df9c3905ce0dd05
hixiaotian <1220219614@qq.com>
2020-05-12 10:26:43 +0800

    update form method

--- Src/Code/course/models.py
+++ Src/Code/course/models.py
@@ -6,7 +6,7 @@ class Course(models.Model):
     begin_time = models.DateTimeField(auto_now_add=True)
     end_time = models.DateTimeField(auto_now_add=True)
     # category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, null=False)
-    form_method = models.CharField(max_length=64, unique=False)
+    form_method = models.IntegerField(default=0, unique=False)
     pic = models.CharField(max_length=64, unique=False, default='default.jpg')
     visit = models.IntegerField(default=0, unique=False)
     description = models.CharField(max_length=256, unique=False)
